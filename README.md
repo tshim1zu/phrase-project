28c97b14a4b7a63c036d6d44e8efe41c51fdf986
tshim1zu <125942443+tshim1zu@users.noreply.github.com>
2023-02-21 14:10:06 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # phrase-project
 To detect new phrases or unknown words for texts in Japanese
+
+
+# Example
+df_texts = pd.read_table("text.tsv", header=None, lineterminator='\n', names=["sentence"])
+jp = jphrase(**params)
+jp.get_dfphrase(df_texts["sentence"])
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jphrase",
+    version="0.1.0",
+    author="Takeshi SHIMIZU",
+    author_email="shim1zu@hotmail.com",
+    description="You can detect new phrases or unknown words for texts in Japanese",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tshim1zu/phrase-project",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+    },
+    python_requires='>=3.7',
+)
